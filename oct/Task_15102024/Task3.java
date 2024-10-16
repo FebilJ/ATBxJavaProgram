349b7775ccf197d8b962ba527f751e3a75829100
fjosebab <fjosebab@FJOSEBAB1.mshome.net>
2024-10-16 22:44:30 +0530

    Commit-Task_15

---
+++ oct/Task_15102024/Task3.java
@@ -0,0 +1,11 @@
+package oct.Task_15102024;
+
+public class Task3 {
+	public static void main(String args[]) {
+		
+		int a = 20;
+
+		System.out.println(--a + a++ + a--);
+		System.out.println(a);
+	}
+}
